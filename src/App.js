2cdcab9bda233006490225677f04c4d6ab50f62c
Rachmat Hidayat <rachmat@hiapp.id>
2019-08-12 13:11:06 +0700

    fix error change language learn react to bahasa react

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bahasa React
+          Learn React
         </a>
       </header>
     </div>
